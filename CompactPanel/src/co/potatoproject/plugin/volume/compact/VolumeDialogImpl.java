9a0e1d066cc669972e6c33854ffd98d55c0b4183
rajadeja <raj.jdja@gmail.com>
2019-03-15 13:23:16 +0000

    Add notification slider if ring & notification is unlinked
    Change-Id: I8a8888e1e8a44e5eff8be2977895d587ddedf83c

--- CompactPanel/src/co/potatoproject/plugin/volume/compact/VolumeDialogImpl.java
+++ CompactPanel/src/co/potatoproject/plugin/volume/compact/VolumeDialogImpl.java
@@ -458,7 +458,9 @@ public class VolumeDialogImpl extends PanelSideAware implements VolumeDialog {
     private void cleanExpandedRows() {
         for (int i = mRows.size() - 1; i >= 0; i--) {
             final VolumeRow row = mRows.get(i);
-            if (row.stream == AudioManager.STREAM_RING || row.stream == AudioManager.STREAM_ALARM) {
+            if ((row.stream == AudioManager.STREAM_RING
+                   || row.stream == AudioManager.STREAM_NOTIFICATION
+                   || row.stream == AudioManager.STREAM_ALARM) && row.stream != mActiveStream) {
                 removeRow(row);
             }
         }
@@ -576,6 +578,11 @@ public class VolumeDialogImpl extends PanelSideAware implements VolumeDialog {
                 == BluetoothProfile.STATE_CONNECTED;
     }
 
+    private boolean shouldShowNotificationStream() {
+        ContentResolver ns = mContext.getContentResolver();
+        return Settings.Secure.getInt(ns, Settings.Secure.VOLUME_LINK_NOTIFICATION, 1) == 1;
+    }
+
     public void initSettingsH() {
         if (mExpandRowsView != null) {
             mExpandRowsView.setVisibility(
@@ -601,6 +608,10 @@ public class VolumeDialogImpl extends PanelSideAware implements VolumeDialog {
                             mSysUIR.drawable("ic_volume_ringer_mute"), true, false);
                     addRow(AudioManager.STREAM_ALARM, mSysUIR.drawable("ic_volume_alarm"),
                             mSysUIR.drawable("ic_volume_alarm_mute"), true, false);
+                if (!shouldShowNotificationStream()) {
+                    addRow(AudioManager.STREAM_NOTIFICATION, mSysUIR.drawable("ic_volume_notification"),
+                            mSysUIR.drawable("ic_volume_notification_mute"), true, false);
+                }
                     updateAllActiveRows();
                     mPanelMode = PanelMode.EXPANDED;
                     updatePanelOnMode();
